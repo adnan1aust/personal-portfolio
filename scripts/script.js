c93f1bebfe5317b25f7c50769e801be7dfbebb89
adnan1aust <adnan1aust@users.noreply.github.com>
2021-06-27 19:44:30 +0600

    Updated Portfolio

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,12 @@
 $(document).on('scroll', function() {
     if($(this).scrollTop()>=$('.about').position().top && $("#html").width() == 0){
-        $("#html").animate({width:"90%"}, 1000);
-        $("#css").animate({width:"80%"}, 1000);
-        $("#js").animate({width:"80%"}, 1000);
-        $("#php").animate({width:"80%"}, 1000);
-        $("#java").animate({width:"80%"}, 1000);
-        $("#salesforce").animate({width:"80%"}, 1000); 
-        $("#ui-ux").animate({width:"80%"}, 1000);  
+        $("#html").animate({width:"100%"}, 1000);
+        $("#css").animate({width:"100%"}, 1000);
+        $("#js").animate({width:"100%"}, 1000);
+        $("#php").animate({width:"100%"}, 1000);
+        $("#java").animate({width:"100%"}, 1000);
+        $("#salesforce-admin").animate({ width: "100%" }, 1000);
+        $("#salesforce-developer").animate({width:"100%"}, 1000);
+        $("#ui-ux").animate({width:"100%"}, 1000);
     }
 })
